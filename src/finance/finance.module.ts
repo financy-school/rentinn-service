0d7a4ff2bcba1e5f041bd6c2bb9006fd38c8f596
sumittguptaa <sumitgupta4535@gmail.com>
2025-04-01 06:09:33 +0530

    fix

--- src/finance/finance.module.ts
+++ src/finance/finance.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FinanceService } from './finance.service';
 import { FinanceController } from './finance.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Finance } from './entities/finance.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Finance])],
   controllers: [FinanceController],
   providers: [FinanceService],
 })
