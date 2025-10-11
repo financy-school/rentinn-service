1082b5c4d1da457307db9e9c43e929c3ba4f6061
sumittguptaa <sumitgupta4535@gmail.com>
2025-10-12 02:28:42 +0530

    Add HttpModule to DocumentsModule imports

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -4,9 +4,14 @@ import { DocumentsController } from './documents.controller';
 import { DocumentEntity } from './entities/document.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DocumentEntity]), ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([DocumentEntity]),
+    ConfigModule,
+    HttpModule,
+  ],
   controllers: [DocumentsController],
   providers: [DocumentsService],
   exports: [DocumentsService],
